f1e0799dc4ce21ce1e8793d9a2d87cf7a01f6cd3
Hakim.Z <110186627+Hakim-2206@users.noreply.github.com>
2025-04-05 16:02:01 +0200

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebSite.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/MyFirstWebSiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebSite.Models;
+
+namespace MyFirstWebSite.Data
+{
+    public class MyFirstWebSiteContext : DbContext
+    {
+        public MyFirstWebSiteContext (DbContextOptions<MyFirstWebSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstWebSite.Models.Customer> Customer { get; set; } = default!;
+    }
+}
